549dadd52ab7f75ff1cbd201f9636641a3d51403
Gad Kadosh <32033534+gadkadosh@users.noreply.github.com>
2023-11-20 17:42:12 +0100

    feat: Duration range input and styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("htmx-request", ["&.htmx-request", ".htmx-request &"]);
+    }),
+  ],
+};
